dbf7bd6787798d6e48f0b834167b140da135811e
puneet0686 <puneet0686@gmail.com>
2025-09-19 14:38:53 +0530

    update file

---
+++ newrgfile.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.37.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "25a58ceb-2570-42e2-9d35-00b46979c51a"
+}
+
+resource "azurerm_resource_group" "rgpipe"{
+ name = "infrarg"
+ location = "Central India"
+}
+
+resource "azurerm_resource_group" "rgpipe"{
+ name = "infrarg"
+ location = "Central India"
+}
+
